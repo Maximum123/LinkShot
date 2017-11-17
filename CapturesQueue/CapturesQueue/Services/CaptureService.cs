bcc7014e4e5e08a532d2669e31292ac99bf43b08
Maximus <nologin77@gmail.com>
2017-11-18 01:59:24 +0200

    up

---
+++ CapturesQueue/CapturesQueue/Services/CaptureService.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Configuration;
+using System.IO;
+using CloudinaryDotNet;
+using CloudinaryDotNet.Actions;
+
+namespace CapturesQueue.Services
+{
+    public class CaptureService : ICaptureService
+    {
+        private readonly Cloudinary _cloudinary;
+
+        public CaptureService()
+        {
+            var name = ConfigurationManager.AppSettings["Clouddinary.Name"];
+            var key = ConfigurationManager.AppSettings["Clouddinary.ApiKey"];
+            var secret = ConfigurationManager.AppSettings["Clouddinary.ApiSecret"];
+            Account account = new Account(
+                name,
+                key,
+                secret);
+
+             _cloudinary = new Cloudinary(account);
+        }
+
+        public string UploadScreenShot(byte[] screenshotBytes)
+        {
+            if (screenshotBytes == null)
+                return String.Empty;
+            using (Stream ms = new MemoryStream(screenshotBytes))
+            {
+                var uploadParams = new ImageUploadParams()
+                {
+                    File = new FileDescription(Guid.NewGuid().ToString(), ms)
+                };
+                var uploadResult = _cloudinary.Upload(uploadParams);
+
+                return uploadResult.Uri.ToString();
+            }
+        }
+    }
+}
