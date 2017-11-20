e694a3b467cdad0e7f99cbfe46d905962eb6691e
volodymyr.liashenko <volodymyr.liashenko@creuna.com>
2017-11-20 18:50:02 +0200

    Delete all files.

--- LinkShort/Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
+++
@@ -1,6 +0,0 @@
-namespace LinkShort.Areas.HelpPage.ModelDescriptions
-{
-    public class DictionaryModelDescription : KeyValuePairModelDescription
-    {
-    }
-}
--- LinkShort/Areas/HelpPage/ModelDescriptions/SimpleTypeModelDescription.cs
+++
@@ -1,6 +0,0 @@
-namespace LinkShort.Areas.HelpPage.ModelDescriptions
-{
-    public class SimpleTypeModelDescription : ModelDescription
-    {
-    }
-}
